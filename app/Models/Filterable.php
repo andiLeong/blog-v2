37a00a1fcdc205ce834a17d6ea9cdd1e207334c9
andiLeong <alwayssmile20150724@gmail.com>
2022-04-29 22:55:49 +0800

    refactor Filterable trait

--- app/Models/Filterable.php
+++ app/Models/Filterable.php
@@ -2,29 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+
 trait Filterable
 {
-    public function scopeFilters($query,array $request = null)
+    public function scopeFilters(Builder $query,array $request = null)
     {
         if(!method_exists($this,'getFilter')){
             return $query;
         }
-        $filters = $this->getFilter();
 
-        if($filters->isEmpty()){
-            return $query;
-        }
-
-        $request = collect($request ?? request()->all());
-
-        $request->filter()->only($filters->keys())->whenNotEmpty(function($collection) use($filters,$query){
-            $filters->filter(function($value,$key) use($collection){
-                return $collection->has($key);
-            })
-            ->values()
-            ->each
-            ->apply($query);
-        });
+        $request = collect($request ?? request()->all())->filter();
+        $this->getFilter()->intersectByKeys($request)->each->apply($query);
 
         return $query;
     }
