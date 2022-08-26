c78db82ffe8f3f2bea5d32ee87e5e85448451579
andiLeong <alwayssmile20150724@gmail.com>
2022-08-26 15:00:25 +0800

    Added a backup command

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,10 @@
 <?php
 
-use App\Foo;
-use App\Models\Order;
-use Illuminate\Database\MySqlConnection;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
-
-    $mysqlConnection = new MySqlConnection($pdo);
-
-    $queryBuilder = new Builder($mysqlConnection);
-
-    $queryBuilder = $queryBuilder
-//        ->select(['id','country','id'])
-        ->from('orders')
-//        ->where('id','>',3)
-            ->whereBetween('amount',[0,9999])
-        ->whereIn('acount',['foo','bar'])
-
-//        ->toSql()
-//        ->get(['id','country'])
-    ;
-
-    dd($queryBuilder);
-    return view('welcome');
-});
-
-
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 
 Route::post('/login', \App\Http\Controllers\LoginController::class);
